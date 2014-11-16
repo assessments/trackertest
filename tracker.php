8af7b71d47866f7294c28d83e63a5d95b58606b4
Mark Macdonald <mark.t.macdonald@googlemail.com>
2014-11-16 21:11:12 +0100

    added initial PHP structure for loading and parsing XML.

--- tracker.php
+++ tracker.php
@@ -6,7 +6,7 @@ set_time_limit(0);
 class Tracker {
 
     private $url;
-    private $response;
+    private $result;
 
     public function __construct($url) {
     	$this->url = $url;
@@ -25,7 +25,7 @@ class Tracker {
 
     public function parse() {
 
-		//http://stackoverflow.com/questions/1835177/
+    	$count = 0;
 
 		$parser = new XMLReader;
 		$parser->open('temp.xml');
@@ -38,15 +38,53 @@ class Tracker {
 		{
 		    $item = new SimpleXMLElement($parser->readOuterXML());
 
-		    echo('product id: '.$item->productID.'<br/>');
+		    $id = $item->productID;
+		    $name = $item->name;
+			$description = $item->description;
+			$price = $item->price;
+			$currency = $item->price['currency'];
+			$url = $item->productURL;
+			$categories = [];
+
+			foreach($item->categories as $category) {
+				array_push($categories, $category->category);
+			}
+
+			/*
+				Product: [PRODUCT_NAME] ([PRODUCT_ID])
+				Description: [PRODUCT_DESCRIPTION]
+				Price: [PRODUCT_CURRENCY] [PRODUCT_PRICE]
+				Categories: [PRODUCT_CATEGORY][,][PRODUCT_CATEGORY][...]
+				URL: [PRODUCT_URL]
+			*/
+
+			$content = 'Product: '.$name.' ('.$id.')'.PHP_EOL;
+			$content .= 'Description: '.$description.PHP_EOL;
+			$content .= 'Price: '.$currency.' '.$price.PHP_EOL;
+			$content .= 'Categories: ';
+			foreach ($categories as $category) {
+				$content .= $category.', ';
+			}
+			//$content = rtrim($content, ",");
+			$content .= PHP_EOL;
+			$content .= 'URL: '.$url.PHP_EOL;
 
 		    // go to next product
 		    $parser->next('product');
+		    $count++;
+
+		    file_put_contents('products/'.$id.'.txt', $content);
 		}
+
+		$this->result = $count;
+    }
+
+    public function writeFile($file, $content) {
+		
     }
 
     public function getResult() {
-    	return $this->response; 
+    	return $this->result; 
     }
 }
 
@@ -54,3 +92,5 @@ class Tracker {
 $url = 'http://pf.tradetracker.net/?aid=1&type=xml&encoding=utf-8&fid=567342&categoryType=2&additionalType=2&limit=10'; //&limit=10
 $tracker = new Tracker($url);
 $tracker->fetch();
+
+echo $tracker->getResult();
