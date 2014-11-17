83d781b31a53ec87c0f01de9262dd603afaa859c
Mark Macdonald <mark.t.macdonald@googlemail.com>
2014-11-17 14:21:59 +0100

    connect UI to back end, style page.

--- index.js
+++ index.js
@@ -1,40 +1,80 @@
+/**
+ * Tracker class
+ */
+
 function Tracker() {
 
-	this.url = '';
+    /**
+     * Models
+     */
 
-	this.submit = function () {
-		this.url = document.getElementById('url').value;
-		
-		this.load();
-	};
+    this.url = '';
 
-	this.load = function () {
+    this.response = {};
 
-		var request = new XMLHttpRequest();
-		request.open('POST', 'tracker.php', true);
-		request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    /**
+     * Controllers
+     */
 
-		request.onload = function() {
-			if (request.status >= 200 && request.status < 400){
-		    	console.log(request.responseText); //success
-		  	} else {
-		  		console.log(request.responseText); //error
-			}
-		};
+    //the submit method is an event handler
+    this.submit = function () {
+        this.url = document.getElementById('url').value;
+        this.load();
+    };
 
-		request.onerror = function() {
-			console.log('connection error');
-		};
+    //the load method uses AJAX and POST to invoke a request to the server
+    this.load = function () {
 
-		request.send('url='+this.url);
-	}
+        var self = this;
+        var request = new XMLHttpRequest();
+        request.open('POST', 'tracker.php', true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 
-	document.getElementById('submit-url').addEventListener('click', this.submit.bind(this), false);
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 400) {
+                self.response = JSON.parse(request.responseText); //success
+            } else {
+                self.response = {count: null, errors: [request.responseText]}; //server error
+            }
+            self.draw();
+        };
+
+        request.onerror = function () {
+            self.response = {count: null, errors: ['Connection error']}; //connection error
+            self.draw();
+        };
+
+        request.send('url='+encodeURIComponent(this.url));
+    }
+
+    //add an event handler for the submit button
+    document.getElementById('submit-url').addEventListener('click', this.submit.bind(this), false);
+
+    /**
+     * Views
+     *
+     * In a larger app, consider a template solution (e.g Handlebars.js) or view framework (e.g. React)
+     */
+
+    //the draw method renders the response to the page
+    this.draw = function () {
+        var html = [], i = -1;
+        if (this.response.count) {
+            html[++i] = '<div class="success">Success: '+this.response.count+' products tracked</div>';
+        } else if (this.response.errors.length > 0) {
+            html[++i] = '<div class="error">';
+            this.response.errors.forEach(function (error) {
+                html[++i] = '<p>'+error+'</p>';
+            }, this);
+            html[++i] = '</div>';
+        }
+        document.getElementById('response').innerHTML = html.join('');
+    }
 }
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function (){
 
-	//instantiate the tracker class
-	var tracker = new Tracker();
+    //instantiate the tracker class
+    var tracker = new Tracker();
 
 });
--- tracker.php
+++ tracker.php
@@ -6,91 +6,93 @@ set_time_limit(0);
 class Tracker {
 
     private $url;
-    private $result;
+    private $response;
 
     public function __construct($url) {
-    	$this->url = $url;
+        $this->url = $url;
+        $this->response = null;
     }
 
     public function fetch() {
-		$fp = fopen ('temp.xml', 'w+');
-		$curl = curl_init($this->url);
-		curl_setopt($curl, CURLOPT_FILE, $fp);
-		curl_exec($curl);
-		curl_close($curl);
-		fclose($fp);
 
-		$this->parse();
+        $fp = fopen ('temp.xml', 'w+');
+        $curl = curl_init($this->url);
+        curl_setopt($curl, CURLOPT_FILE, $fp);
+        curl_exec($curl);
+        curl_close($curl);
+        fclose($fp);
+
+        $this->parse();
     }
 
     public function parse() {
 
-    	$count = 0;
+        $count = 0;
 
-		$parser = new XMLReader;
-		$parser->open('temp.xml');
+        $parser = new XMLReader;
+        $parser->open('temp.xml');
 
-		// move to the first product node
-		while ($parser->read() && $parser->name !== 'product');
+        // move to the first product node
+        while ($parser->read() && $parser->name !== 'product');
 
-		// iterate each product
-		while ($parser->name === 'product')
-		{
-		    $item = new SimpleXMLElement($parser->readOuterXML());
+        // iterate each product
+        while ($parser->name === 'product')
+        {
+            $item = new SimpleXMLElement($parser->readOuterXML());
 
-		    $id = $item->productID;
-		    $name = $item->name;
-			$description = $item->description;
-			$price = $item->price;
-			$currency = $item->price['currency'];
-			$url = $item->productURL;
-			$categories = [];
+            $id = $item->productID;
+            $name = $item->name;
+            $description = $item->description;
+            $price = $item->price;
+            $currency = $item->price['currency'];
+            $url = $item->productURL;
+            $categories = [];
 
-			foreach($item->categories as $category) {
-				array_push($categories, $category->category);
-			}
+            foreach($item->categories as $category) {
+                array_push($categories, $category->category);
+            }
 
-			/*
-				Product: [PRODUCT_NAME] ([PRODUCT_ID])
-				Description: [PRODUCT_DESCRIPTION]
-				Price: [PRODUCT_CURRENCY] [PRODUCT_PRICE]
-				Categories: [PRODUCT_CATEGORY][,][PRODUCT_CATEGORY][...]
-				URL: [PRODUCT_URL]
-			*/
+            /*
+                Product: [PRODUCT_NAME] ([PRODUCT_ID])
+                Description: [PRODUCT_DESCRIPTION]
+                Price: [PRODUCT_CURRENCY] [PRODUCT_PRICE]
+                Categories: [PRODUCT_CATEGORY][,][PRODUCT_CATEGORY][...]
+                URL: [PRODUCT_URL]
+            */
 
-			$content = 'Product: '.$name.' ('.$id.')'.PHP_EOL;
-			$content .= 'Description: '.$description.PHP_EOL;
-			$content .= 'Price: '.$currency.' '.$price.PHP_EOL;
-			$content .= 'Categories: ';
-			foreach ($categories as $category) {
-				$content .= $category.', ';
-			}
-			//$content = rtrim($content, ",");
-			$content .= PHP_EOL;
-			$content .= 'URL: '.$url.PHP_EOL;
+            $content = 'Product: '.$name.' ('.$id.')'.PHP_EOL;
+            $content .= 'Description: '.$description.PHP_EOL;
+            $content .= 'Price: '.$currency.' '.$price.PHP_EOL;
+            $content .= 'Categories: ';
+            foreach ($categories as $category) {
+                $content .= $category.', ';
+            }
+            //$content = rtrim($content, ",");
+            $content .= PHP_EOL;
+            $content .= 'URL: '.$url.PHP_EOL;
 
-		    // go to next product
-		    $parser->next('product');
-		    $count++;
+            // go to next product
+            $parser->next('product');
+            $count++;
 
-		    file_put_contents('products/'.$id.'.txt', $content);
-		}
+            file_put_contents('products/'.$id.'.txt', $content);
+        }
 
-		$this->result = $count;
+        $this->response = $count;
     }
 
-    public function writeFile($file, $content) {
-		
-    }
-
-    public function getResult() {
-    	return $this->result; 
+    public function response() {
+        $response = (object)[
+            'count' => $this->response,
+            'errors' => ''
+        ];
+        return json_encode($response); 
     }
 }
 
-//echo $_GET["url"];
-$url = 'http://pf.tradetracker.net/?aid=1&type=xml&encoding=utf-8&fid=567342&categoryType=2&additionalType=2&limit=10'; //&limit=10
+$url = urldecode($_POST["url"]);
+//$url = 'http://pf.tradetracker.net/?aid=1&type=xml&encoding=utf-8&fid=567342&categoryType=2&additionalType=2&limit=10'; //&limit=10
 $tracker = new Tracker($url);
 $tracker->fetch();
 
-echo $tracker->getResult();
+echo $tracker->response();
